18dd8e8d248bbd9a82fbc520f2e94ff66ea774c0
Yisus2299 <jmzm08@gmail.com>
2026-06-11 21:15:03 -0400

    Translate comments and variable names to English for consistency

--- parte18_POO_generarCosas.py
+++ parte18_POO_generarCosas.py
@@ -1,78 +1,78 @@
-#turtle y interfaz grafica (GUI) video 130
+# turtle and graphical interface (GUI) video 130
 
 from turtle import Turtle, Screen, colormode, pen, speed, circle
 import random
 
 import heroes
 
-tortuga = Turtle()
-tortuga.shape("turtle")
-tortuga.color("black", "orange")
+turtle = Turtle()
+turtle.shape("turtle")
+turtle.color("black", "orange")
 
 #========================================================================================================================================#
 
-''' 1 - #si queremos dibujar figuras que vayan en diferentes angulos debemos de crear variables
+''' 1 - # if we want to draw shapes that go at different angles, we must create variables
 
-#podemos colocar el numero de angulos a recorrer estaticamente como: num_sides = 5
-# pero si realmente queremos que el numero no sea estatico y quee el numero cambie, es con una funcion
+# we can set the number of sides to traverse statically like: num_sides = 5
+# but if we really want the number not to be static and for the number to change, we use a function
 
-colors = ['Green','Red','Orange','Yellow','Purple','IndianRed','wheat','SeaGreen'] #creamos una variable para almacenar los colores
+colors = ['Green','Red','Orange','Yellow','Purple','IndianRed','wheat','SeaGreen'] # create a variable to store the colors
 
 def forma(num_sides):
-    angle = 360 / num_sides #sacamos esta variable afuera para tener diferentes formas
+    angle = 360 / num_sides # compute this variable to create different shapes
     for i in range(num_sides):
-        tortuga.forward(100)
-        tortuga.right(angle)
+        turtle.forward(100)
+        turtle.right(angle)
 
 for shape_side in range(3,11):
-    tortuga.color(random.choice(colors)) #importamos random para que eliga los colores aleatoriamente
-    forma(shape_side) #hacemos otro bucle For y le metemos adentro la funcion para hacer otra figura'''
+    turtle.color(random.choice(colors)) # import random so it chooses colors randomly
+    forma(shape_side) # create another for loop and call the function to draw another shape'''
 
 #==========================================================================================================================================#
 
 #2- Random walk
 
-'''colormode(255) #en lugar de nosotros poner los colores como en la pasada. Existe este atributo que nos permite asignarlo de manera aleatoria
+'''colormode(255) # instead of setting the colors manually like before, this attribute allows us to assign them randomly
 
-def random_color(): #creamos una funcion para evitar repetir el codigo mil veces xd
+def random_color(): # create a function to avoid repeating the code a thousand times
     r = random.randint(0, 255)
     g = random.randint(0, 255)
     b = random.randint(0, 255)
-    random_color= (r,g,b) # y almacenamos todo en una variable con una tupla y lo regresamos
+    random_color = (r, g, b) # store everything in a tuple and return it
     return random_color
 
 
-direccion = [0, 90, 180, 270] #cremos unas direcciones aleartorias
-tortuga.pensize(10) #esto aumenta el tamano de las lineas que la tortuga dibuja
-tortuga.speed(10) #aumenta la velocidad dela tortuga coloreando
+directions = [0, 90, 180, 270] # create some random directions
+turtle.pensize(10) # this increases the thickness of the lines the turtle draws
+turtle.speed(10) # increases the drawing speed of the turtle
 
 for _ in range(200):
-    tortuga.color(random_color())
-    tortuga.forward(30)
-    tortuga.setheading(random.choice(direccion))'''
+    turtle.color(random_color())
+    turtle.forward(30)
+    turtle.setheading(random.choice(directions))'''
 
 #===========================================================================================================================================#
 
-# 3 - dibujar un Spirografo
+# 3 - draw a Spirograph
 
 colormode(255)
-tortuga.speed("fastest") #mantenemos que el sistema eliga los colores aleatoriamente y la velocidad con la que los pinta
+turtle.speed("fastest") # keep the system choosing colors randomly and control the speed at which it paints
 
-def random_color(): #mantenemos la funcion de los colores
+def random_color(): # keep the color function
     r = random.randint(0, 255)
     g = random.randint(0, 255)
     b = random.randint(0, 255)
-    colores = (r,g,b)
-    return colores
+    colors = (r, g, b)
+    return colors
 
-def spirograph(gap): #creamos una funcion en donde el espaciado sea minimo o mas grande, depende de nosotros
-    for _ in range(int(360 / gap)): #usamos un bucle For para que lo haga automaticamente sin necesidad de que nosotros pongamos linea tras linea
-        tortuga.color(random_color()) #color aleatorio
-        tortuga.circle(100) #generamos un circulo una y otra, y otra, y otra
-        tortuga.setheading(tortuga.heading() + gap)
-# tortuga.setheading(...) fija la nueva dirección con ese resultado
-#tortuga.heading() obtiene el ángulo actual (por ejemplo 0, 90, 180...).
-#+ gap le suma un incremento (por ejemplo 10 grados).
+def spirograph(gap): # create a function where the spacing can be smaller or larger, depending on us
+    for _ in range(int(360 / gap)): # use a for loop so it runs automatically without writing line after line
+        turtle.color(random_color()) # random color
+        turtle.circle(100) # draw a circle again and again
+        turtle.setheading(turtle.heading() + gap)
+# turtle.setheading(...) sets the new direction using that result
+# turtle.heading() gets the current angle (for example 0, 90, 180...).
+# + gap adds an increment (for example 10 degrees).
 spirograph(5)
 
 
@@ -96,15 +96,15 @@ spirograph(5)
 
 
 
-pantalla = Screen()
-pantalla.exitonclick()
 
 
 
 
+screen = Screen()
+screen.exitonclick()
 
 
 
-# https://pypi.org/project : para importar cosas y leer documentacion tenemos la pagina de piyi
+# https://pypi.org/project : to import packages and read documentation we use the pypi page
 # import heroes
 # print (heroes.genarr(10))
