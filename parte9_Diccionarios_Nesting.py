8f80875cc96973c5bcc31fd531ac8945dce01b3a
Yisus2299 <jmzm08@gmail.com>
2026-06-11 20:36:13 -0400

    Translate comments and variable names to English for consistency

--- parte9_Diccionarios_Nesting.py
+++ parte9_Diccionarios_Nesting.py
@@ -1,34 +1,33 @@
-# Diccionarios y Nesting
+# Dictionaries and Nesting
 
-#para usar los diccionarios creados tenemos que llamarlos y usar los [] para indicar cual es
+# to use the created dictionaries we have to call them and use [] to indicate which one it is
 programing_dictionary = {
- "Function": " 1 - A piece of code that you can easily call over and over again.",
- "Loop": "2- The action of doing something over and over again.",
+ "Function": "1 - A piece of code that you can easily call over and over again.",
+ "Loop": "2 - The action of doing something over and over again.",
 }
 
-#asi podemos agregar cosas de manera externa a los diccionarios
-programing_dictionary["Bug"] = "3- An error in a program that prevents the program from running as expected."
-programing_dictionary["Practica"] = "4- Una practica a ver en donde se agrega esto al diccionario."
+# this is how we can add items externally to dictionaries
+programing_dictionary["Bug"] = "3 - An error in a program that prevents the program from running as expected."
+programing_dictionary["Practice"] = "4 - A practice entry to see where this is added to the dictionary."
 
 # print(programing_dictionary)
 
 empty_dictionary = {}
-empty_dictionary["existing"] = "algo para saber que la lista no esta vacia"
+empty_dictionary["existing"] = "something to know that the list is not empty"
 # print(empty_dictionary["existing"])
 
-#2- editar un item en un diccionario
+#2- edit an item in a dictionary
 
-programing_dictionary["Bug"] = "se cambio el contenido del Bug"
+programing_dictionary["Bug"] = "the content of Bug was changed"
 #print(programing_dictionary)
 
-#3- Bucle a traves de un diccionario: el for muestra todo lo que hay en el diccionario
+#3- Loop through a dictionary: the for shows everything in the dictionary
 # for _ in programing_dictionary:
     # print(_)
 
-#4- ejercicio
-# de los nombres de los estudiantes con los puntajes que tenemos
-# necesitamos saber quien tiene mejor nota, para eso usamos condicionales para comparar los puntajes
-
+#4- exercise
+# from the student names with the scores we have
+# we need to know who has the better grade, so we use conditionals to compare the scores
 
 student_scores = {
     'Harry': 88,
@@ -40,33 +39,33 @@ student_scores = {
 
 student_grades = {}
 
-#creamos un bucle for con las variables de nombres y puntajes
-# el cual recorrera todo el diccionario de puntajes
+# create a for loop with the name and score variables
+# which will traverse the entire scores dictionary
 for nombre, puntaje in student_scores.items():
     if puntaje >= 91:
-        student_grades[nombre] = "Sobresaliente"
+        student_grades[nombre] = "Outstanding"
     elif puntaje >= 81:
-        student_grades[nombre] = "Puedes mejorar"
+        student_grades[nombre] = "You can improve"
     elif puntaje >= 71:
-        student_grades[nombre] = "Nada mal"
+        student_grades[nombre] = "Not bad"
     else:
-        student_grades[nombre] = "Fallaste"
+        student_grades[nombre] = "Failed"
 
 #print(student_grades)
 
-#Nesting
-#es la combinacion de una lista con un diccionario
+# Nesting
+# it is the combination of a list with a dictionary
 
 viajes = {
-    "Francia": {
-        "ciudades visitadas": ["Paris","Lille","Dijon"],
+    "France": {
+        "visited cities": ["Paris","Lille","Dijon"],
         "total": 12
     },
     "Germany": {
-        "ciudades visitadas": ["Berlin", "Stuttgart"],
+        "visited cities": ["Berlin", "Stuttgart"],
         "total": 5
     },
 }
 
-'''print(viajes["Germany"]["ciudades visitadas"][1])''' #si queremos mostrar algo de una sola cosa, seleccionamos la rama principal y luego lo que queremos mostrar
+'''print(viajes["Germany"]["visited cities"][1])''' # if we want to show a single item, we select the main branch and then what we want to display
 
